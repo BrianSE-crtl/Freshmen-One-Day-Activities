68d072241b06102b08e91e5131fcf126fd937795
BrianSE-crtl <brianle231@gmail.com>
2026-09-01 09:10:37 -0400

    kb

--- Charade.py
+++ Charade.py
@@ -7,7 +7,7 @@ os.system(
 randStuff = [
     "Chopping down wood",
     "Plunging a toilet",
-    "A grandma using TikTok",
+    "A grandma/grandpa using TikTok",
     "Getting attacked by bees",
     "Trying to catch a fly",
     "Lost in a desert",
@@ -15,17 +15,22 @@ randStuff = [
     "Eating extremely spicy food",
     "Milking a cow",
     "Walking on a rope",
-    "A dog chasing its tail",
     "Eating noodles with chopsticks",
-    "Finding your car in a huge parking lot",
     "A snowball fight",
     "Doing karaoke",
     "Juggling oranges",
-    "Pretending to be a zombie",
+    "Walking Zombie",
     "Inflating a balloon with your mouth",
     "Sneaking a snack",
     "Eating a burger",
-    "Singing a birhday song",
-    "Using a toaster"
+    "Using a toaster",
+    "Trying to open a jar",
+    "Walking like a penguin", 
+    "Playing the guitar", 
+    "Lifting weights",
+    "Having a pillowfight",
+    "Reading a newspaper",
+    "Taking a nap",
+    "Making your bed"
 ]
 print(f"Your prompt is: \n{random.choice(randStuff)}\n\n")
