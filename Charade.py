923f3d73dd36d934cfdd073a34b04029ee8b3f0f
BrianSE-crtl <brianle231@gmail.com>
2026-09-02 10:33:10 -0400

    added more prompts

--- Charade.py
+++ Charade.py
@@ -1,9 +1,7 @@
 import random
 import os
-os.system(
-    'cls' if os.name == 'nt'
-    else 'clear'
-    )
+import time
+os.system('cls' if os.name == 'nt'else 'clear')
 randStuff = [
     "Chopping down wood",
     "Plunging a toilet",
@@ -14,11 +12,11 @@ randStuff = [
     "A baby learning to walk",
     "Eating extremely spicy food",
     "Milking a cow",
-    "Walking on a rope",
+    "Walking on a tightrope",
     "Eating noodles with chopsticks",
     "A snowball fight",
     "Doing karaoke",
-    "Juggling oranges",
+    "Juggling balls",
     "Walking Zombie",
     "Inflating a balloon with your mouth",
     "Sneaking a snack",
@@ -31,6 +29,8 @@ randStuff = [
     "Having a pillowfight",
     "Reading a newspaper",
     "Taking a nap",
-    "Making your bed"
 ]
-print(f"Your prompt is: \n{random.choice(randStuff)}\n\n")
+while True:
+    print(f"Your prompt is: \n{random.choice(randStuff)}\n\n")
+    time.sleep(60)
+    os.system('cls' if os.name == 'nt'else 'clear')
